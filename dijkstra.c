1b7c18966ddc596ba5907ae2490a72ef14331142
GeneralGuy4872 </dev/null>
2019-09-08 19:15:32 -0500

    push commit from localhost

--- dijkstra.c
+++ dijkstra.c
@@ -1,11 +1,18 @@
-datakeeper = makepathdata(pointa.x,pointa.y,pointa.z)	//set grid to -1.0,0,false,false,false
+dijkstra(ortho,mode,runningnorm,tile,pointa,pointb,fly,swim)
+bool ortho
+schar mode
+uchar runningnorm
+schar tile
+coord3 pointa
+coord3 pointb
+bool fly
+bool swim
+{
+datakeeper = makepathdata(pointa.x,pointa.y,pointa.z)
 
 datakeeper→grid[COORDSUB(pointa.z,pointa.y,pointa.x + 1)].runningdist = 0.0
-datakeeper→grid[COORDSUB(pointa.z,pointa.y,pointa.x + 1)].up = true
-datakeeper→grid[COORDSUB(pointa.z,pointa.y,pointa.x + 1)].fwd = true
-datakeeper→grid[COORDSUB(pointa.z,pointa.y,pointa.x + 1)].down = true
 
-setpathobs(&datakeeper,fly,swim)
+setpathobs(&datakeeper,mode,fly,swim)
 
 setcoord3* current = datakeeper→hydra_ptr→next
 setcoord3* last = datakeeper→hydra_ptr→next
@@ -641,12 +648,55 @@ forever {
 		}
 	}
 breakout:
+coord3 currentcoord
+currentcoord.x = current→x
+currentcoord.y = current→y
+currentcoord.z = current→z
+
+current = datakeeper→hydra_ptr→prev
+setcoord3* previous = datakeeper→hydra_ptr→prev→prev
+datakeeper→hydra_ptr→next = NULL
+datakeeper→hydra_ptr→prev = NULL
+while (previous != NULL) {
+	free (current)
+	current = previous
+	previous = previous→prev
+	}
+free(current)
+current = NULL
+
 setcoord3* coordreturn_tail = malloc(sizeof(setcoord3))
 coordlistail→next = coordlistail
 coordlistail→prev = coordlistail
 coordlistail→x = current→x
 coordlistail→y = current→y
 coordlistail→z = current→z
-setcoord3* coordreturn_head
+setcoord3* coordreturn_head = coordreturn_tail
+setcoord3* coordreturn_new
+coord3 nextcoord
 
-while (! ((current.
+while (!((currentcoord.x == pointa.x) && (currentcoord.y == pointa.y) && (currentcoord.z == pointa.z))) {
+	//extrapolate origin from trajectory
+	nextcoord.z = currentcoord.z + datakeeper→grid[COORDSUB(currentcoord.z,currentcoord.y,currentcoord.x)].down - datakeeper→grid[COORDSUB(currentcoord.z,currentcoord.y,currentcoord.x)].up
+	if (datakeeper→grid[COORDSUB(currentcoord.z,currentcoord.y,currentcoord.x)].fwd) {
+		switch datakeeper→grid[COORDSUB(currentcoord.z,currentcoord.y,currentcoord.x)].az : {
+			case 1 : nextcoord.y = currentcoord.y - 1; nextcoord.x = currentcoord.x + 1; break;
+			case 2 : nextcoord.y = currentcoord.y; nextcoord.x = currentcoord.x + 1; break;
+			case 3 : nextcoord.y = currentcoord.y + 1; nextcoord.x = currentcoord.x + 1; break;
+			case 4 : nextcoord.y = currentcoord.y + 1; nextcoord.x = currentcoord.x; break;
+			case 5 : nextcoord.y = currentcoord.y + 1; nextcoord.x = currentcoord.x - 1; break;
+			case 6 : nextcoord.y = currentcoord.y; nextcoord.x = currentcoord.x - 1; break;
+			case 7 : nextcoord.y = currentcoord.y - 1; nextcoord.x = currentcoord.x - 1; break; 
+			default : nextcoord.y = currentcoord.y - 1; nextcoord.x = currentcoord.x; break;
+			}
+		}
+	currentcoord = nextcoord
+	if (!datakeeper→grid[COORDSUB(currentcoord.z,currentcoord.y,currentcoord.x)].ex) {
+		coordreturn_new = malloc(sizeof(setcoord3))
+		coordreturn_head→prev = coordreturn_new
+		coordreturn_tail→next = coordreturn_new
+		coordreturn_head = coordreturn_new
+		}
+	}
+return coordreturn_head
+}
